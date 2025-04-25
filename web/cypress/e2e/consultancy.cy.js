2b802dd8468a9fa2ffd50613d614f836b48485c7
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-04-25 07:00:37 -0300

    Acessando a tela de consultoria

---
+++ web/cypress/e2e/consultancy.cy.js
@@ -0,0 +1,9 @@
+/// <reference types="cypress" />
+
+describe('Formulário de Consultoria', () => {
+  it('Deve solicitar consultoria individual', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
+    cy.goTo('Formulários', 'Consultoria')
+  })
+})
