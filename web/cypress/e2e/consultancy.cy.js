883c8f810c6fbcf10a86d567bbef5040b99c98b5
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-04-27 08:51:26 -0300

    Adicionando novos seletores

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -7,5 +7,18 @@ describe('Formulário de Consultoria', () => {
     cy.goTo('Formulários', 'Consultoria')
 
     cy.get('input[placeholder*=nome]').type('Leonardo Padilha')
+    cy.get('input[placeholder="Digite seu email"]').type('leonardo@webdojo.com')
+    
+    cy.get('input[placeholder="(00) 00000-0000"]')
+      .type('99 99999-9999')
+      .should('have.value', '(99) 99999-9999') // O número com a máscára é exibido no value do campo
+
+    // cy.get('#consultancyType').select('inCompany')
+    // //label[text()="Tipo de Consultoria"]/..//select
+    cy.contains('label', 'Tipo de Consultoria')
+      .parent()
+      .find('select')
+      .select('Individual')
+
   })
 })
