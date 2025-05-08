4c624bdf602909e939beb912fb2a3d531a7fa164
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-05-08 06:51:14 -0300

    Verificando campos obrigatórios

--- web/cypress/e2e/consultancy.cy.js
+++ web/cypress/e2e/consultancy.cy.js
@@ -79,4 +79,29 @@ describe('Formulário de Consultoria', () => {
       cy.contains('Sua solicitação de consultoria foi enviada com sucesso! Em breve, nossa equipe entrará em contato através do email fornecido')
         .should('be.visible')
   })
+  
+  it('Deve verificar os campos obrigatórios', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
+    cy.goTo('Formulários', 'Consultoria')
+
+    cy.contains('button', 'Enviar formulário')
+      .click()
+
+    cy.contains('p', 'Digite nome e sobrenome')
+      .should('be.visible')
+      .and('have.class', 'text-red-400')
+      .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+    cy.contains('p', 'Informe um email válido')
+      .should('be.visible')
+      .and('have.class', 'text-red-400')
+      .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+    cy.contains('p', 'Você precisa aceitar os termos de uso')
+      .should('be.visible')
+      .and('have.class', 'text-red-400')
+      .and('have.css', 'color', 'rgb(248, 113, 113)')
+
+  })
 })
