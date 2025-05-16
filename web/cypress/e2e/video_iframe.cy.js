67180596d89f0c3e7d093897cbb3e6d667186cdb
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-05-16 13:00:31 -0300

    Iframe

---
+++ web/cypress/e2e/video_iframe.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Tocar o vídeo', () => {
+  it('Deve poder tocar o vídeo dentro de um iframe como exemplo', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
+    cy.contains('Video').click()
+
+    cy.get('iframe[title="Video Player"]')
+      .should('exist')
+      .its('0.contentDocument.body') //Função do cypress utilizada para obter propriedades dos elementos e outros
+      .then(cy.wrap)
+      .as('iFramePlayer')
+
+    cy.get('@iFramePlayer')
+      .find('.play-button')
+      .click()
+
+    cy.get('iFramePlayer')
+      .find('pause-button')
+      .should('be.visible')
+  })
+})
