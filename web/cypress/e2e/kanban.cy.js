25a5c40e4cb9d40465b5568daf3aac2584f5c7db
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-05-17 23:26:37 -0300

    Drag and drop

---
+++ web/cypress/e2e/kanban.cy.js
@@ -0,0 +1,23 @@
+/// <reference types="cypress" />
+
+describe('Kanban Board', () => {
+  it('Deve mover uma tarefa de Todo para Done e atualizar o board', () => {
+    cy.start()
+    cy.submitLoginForm('papito@webdojo.com', 'katana123')
+    cy.contains('Kanban').click()
+
+    const dataTransfer = new DataTransfer()
+
+    cy.contains('div[draggable=true]', 'Documentar API')
+      .trigger('dragstart', { dataTransfer }) // Simulando click e arrastar
+
+    cy.get('.column-done')
+      .trigger('drop', { dataTransfer }) // Soltar o card na coluna
+      .find('h3')
+      .should('have.text', 'Done (4)')
+
+    cy.get('.column-done')
+      .should('include.text', 'Documentar API')
+      .and('include.text', 'Criar documentação da API com Swagger')
+  })
+})
