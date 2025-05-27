344290ffbd9f94ee85f7fb248f1499886be653c1
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-05-27 05:51:30 -0300

    Validando link do github

--- web/cypress/e2e/github.cy.js
+++ web/cypress/e2e/github.cy.js
@@ -56,4 +56,27 @@ describe('Gerenciamento de Perfis no GitHub', () => {
     cy.contains('table tbody', profile.username)
       .should('not.exist')
   })
+
+  it('Deve validar o link do github', () => {
+    const profile = {
+      name: 'Leonardo Padilha',
+      username: 'leonardopadilha',
+      desc: 'QA'
+    }
+
+    cy.get('#name').type(profile.name)
+    cy.get('#username').type(profile.username)
+    cy.get('#profile').type(profile.desc)
+
+    cy.contains('button', 'Adicionar Perfil').click()
+
+    cy.contains('table tbody tr', profile.username)
+      .should('be.visible')
+      .as('trProfile')
+
+    cy.get('@trProfile')
+      .find('a')
+      .should('have.attr', 'href', `https://github.com/${profile.username}`)
+      .and('have.attr', 'target', '_blank')
+  })
 })
