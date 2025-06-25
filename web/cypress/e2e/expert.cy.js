ff7df7bdc9889df53a5557c9253a336d49ff3151
Leonardo Padilha <leonardopadilha28@gmail.com>
2025-06-25 06:42:33 -0300

    Utilização do invoke, simulando Tab, salvando o html da página

--- web/cypress/e2e/expert.cy.js
+++ web/cypress/e2e/expert.cy.js
@@ -1,4 +1,6 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
+import _ from 'lodash'
 
 describe('Expert', () => {
   beforeEach(() => {
@@ -21,7 +23,53 @@ describe('Expert', () => {
     cy.contains('button', 'Entrar')
       .invoke('show')
       .should('be.visible')
+  })
 
+  it('Não deve logar com senha inválida', () => {
+    // docs.cypress.io/api/commands/type
+    cy.get('#email').type('papito@webdojo.com')
+    cy.get('#password').type('asdfasdf{Enter}')
 
+    // Salvando o html da página
+/*     cy.wait(2500)
+    cy.document().then((doc) => {
+      cy.writeFile('cypress/downloads/page.html', doc.documentElement.outerHTML)
+    }) */
+
+    cy.get('[data-sonner-toaster="true"]')
+      .should('be.visible')
+      .as('toast')
+
+    cy.get('@toast')
+      .find('.title')
+      .should('have.text', 'Acesso negado! Tente novamente.')
+
+    cy.wait(5000)
+
+    cy.get('@toast')
+      .should('not.exist')
+  })
+
+  it('Simulando a tecla TAB com cy.press()', () => {
+    cy.get('body').press('Tab')
+    cy.focused().should('have.attr', 'id', 'email')
+
+    cy.get('#email').press('Tab')
+    cy.focused()
+    cy.focused().should('have.attr', 'id', 'password')
+
+  })
+
+  it('Deve realizar uma carga de dados fakes', () => {
+
+    _.times(5, () => {
+      const name = faker.person.fullName()
+      const email = faker.internet.email()
+      const password = 'pwd'
+
+      cy.log(name)
+      cy.log(email)
+      cy.log(password)
+    })
   })
 })
